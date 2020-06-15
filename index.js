d4c1351cead8bb98935c523752864da543e1dab1
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-15 11:10:38 -0600

    add new Timer class

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ class Timer {
 
   }
 }
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
