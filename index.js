3126fbd34aa61d8c3920be217c30d5462f345eec
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-15 10:52:49 -0600

    psuedo code

---
+++ index.js
@@ -0,0 +1,8 @@
+// Event listener to watch for a click on 'start button
+// Draw a full border around the timer
+// Start counting down the timer
+// Each time the timer counts down, update the border
+// Each time the timer counts down, update the text
+// If we counted down and timer reaches 0
+// Reset the border
+// Reset the internal timer to get ready for another run
